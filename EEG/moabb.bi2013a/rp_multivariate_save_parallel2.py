629fa65930d232fd0a58182b437caf5f25099533
toncho11 <toncho11@gmail.com>
2021-12-10 17:08:55 +0100

    update

--- EEG/moabb.bi2013a/rp_multivariate_save_parallel2.py
+++ EEG/moabb.bi2013a/rp_multivariate_save_parallel2.py
@@ -16,6 +16,10 @@ from sklearn.preprocessing import LabelEncoder
 import Dither
 import os
 import glob
+
+from joblib import Parallel, delayed
+from multiprocessing import Manager, Value
+import threading
 import time
 
 import warnings
@@ -31,6 +35,14 @@ le = LabelEncoder()
 
 #bi2013a: FP1, FP2, F5, AFz, F6, T7, Cz, T8, P7, P3, Pz, P4, P8, O1, Oz, O2
 
+print("Start declaring Parallel manager")
+if __name__ == '__main__':
+    epochs_class_1 = Value('i', 0)
+    epochs_class_2 = Value('i', 0)
+
+lock = threading.Lock()
+print("End declaring Parallel manager")
+        
 def multivariateRP(sample, electrodes, dimension, time_delay, percentage):
     
     channels_N = sample.shape[0]
@@ -80,10 +92,44 @@ def multivariateRP(sample, electrodes, dimension, time_delay, percentage):
     
     return X_rp #out
 
+def Process(sample_i, sample, y, folder, subject, m, tau , electrodes, percentage, max_epochs_per_subject):
+
+    print("In Process")
+    label = y[sample_i]
+
+   
+    #lock = threading.Lock()
+    lock.acquire()
+    
+    save = False;
+    
+    if (label==0 and epochs_class_1.value < max_epochs_per_subject) or (label==1 and epochs_class_2.value < max_epochs_per_subject):
+        
+        if (label == 0):
+            epochs_class_1.value = epochs_class_1.value + 1
+     
+        if (label == 1):
+            epochs_class_2.value = epochs_class_2.value + 1
+            
+        save = True
+    
+    lock.release()
+    
+    if (save):
+        
+        single_epoch_subject_rp = multivariateRP(sample, electrodes, m, tau, percentage)
+        filename = "subject_" + str(subject - 1) + "_rp_label_" + str(label) + "_epoch_" + str(sample_i)
+        full_filename = folder + "\\" + filename
+    
+        print("Saving: " + full_filename)
+        # plt.imshow(single_epoch_subject_rp, cmap = plt.cm.binary)
+        np.save(full_filename, single_epoch_subject_rp)
+
 def CreateData(m, tau , filter_fmin, filter_fmax, electrodes, n_subjects, percentage, max_epochs_per_subject):
     
-    folder = "D:\Work\ML_examples\EEG\moabb.bi2013a\data"
-    
+    folder = "C:\\Work\PythonCode\\ML_examples\\EEG\\moabb.bi2013a\\data"
+    #folder = "r:\\data"
+
     folder = folder + "\\rp_m_" + str(m) + "_tau_" + str(tau) + "_f1_"+str(filter_fmin) + "_f2_"+ str(filter_fmax) + "_el_" + str(len(electrodes)) + "_nsub_" + str(n_subjects) + "_per_" + str(percentage) + "_nepo_" + str(max_epochs_per_subject) 
     
     print(folder)
@@ -100,13 +146,17 @@ def CreateData(m, tau , filter_fmin, filter_fmax, electrodes, n_subjects, percen
         
     print("Write rp image data:")
     
+
     
     for dataset in datasets:
         
         for subject_i, subject in enumerate(dataset.subject_list[0:n_subjects]):
             
-            epochs_class_1 = 0
-            epochs_class_2 = 0
+
+            lock.acquire()
+            epochs_class_1.value = 0
+            epochs_class_2.value = 0
+            lock.release();
             print("Loading subject:" , subject)  
             X, y, _ = paradigm.get_data(dataset=dataset, subjects=[subject])
             y = le.fit_transform(y)
@@ -116,29 +166,11 @@ def CreateData(m, tau , filter_fmin, filter_fmax, electrodes, n_subjects, percen
             print("Class target samples: ", sum(y))
             print("Class non-target samples: ", len(y) - sum(y))
             
-            for sample_i,sample in enumerate(X):
-                
-                label = y[sample_i]
-                
-                if (label==0 and epochs_class_1 < max_epochs_per_subject) or (label==1 and epochs_class_2 < max_epochs_per_subject):
-    
-                    single_epoch_subject_rp = multivariateRP(sample, electrodes, m, tau, percentage)
-                    
-                    filename = "subject_" + str(subject-1) + "_rp_label_" + str(label) + "_epoch_" + str(sample_i)
-                    full_filename = folder + "\\" + filename
-                    
-                    print("Saving: " + full_filename)
-                    #plt.imshow(single_epoch_subject_rp, cmap = plt.cm.binary)
-                    np.save(full_filename, single_epoch_subject_rp)
-                    
-                    if (label==0):
-                        epochs_class_1 = epochs_class_1 + 1
-                        
-                    if (label==1):
-                        epochs_class_2 = epochs_class_2 + 1
-                
-            print(epochs_class_1, epochs_class_2)
 
+            #def Process(sample_i, X, y, folder, subject, m, tau, electrodes, percentage):
+            Parallel(n_jobs=24, backend = "threading")(delayed(Process)(sample_i, sample, y, folder, subject, m, tau, electrodes, percentage, max_epochs_per_subject) for sample_i, sample in enumerate(X))
+
+        
 f1 = paradigm.filters[0][0]
 f2 = paradigm.filters[0][1]
 
