f61dba21bcff6bf8f9320738814d3ca37c077c07
Anton <toncho11@gmail.com>
2022-02-25 14:37:14 +0100

    update

--- EEG/P300Analysis/P300ShowData.py
+++ EEG/P300Analysis/P300ShowData.py
@@ -10,41 +10,10 @@ import os
 from glob import glob
 from os import walk
 
+from DatasetHelper import *
+
 #config
 base_folder = "h:" + os.path.sep + "data" + os.path.sep
-
-def GetCzChannel(dataset):
-    if (dataset == "BNCI2014008"): #8 electrodes
-        return 1
-    else:
-        print("Error: Could not set Cz for this dataset!")
-        
-def GetElectrodeCount(dataset):
-    if (dataset == "BNCI2014008"): #8 electrodes
-        return len(GetChannelNames(dataset))
-    else:
-        print("Error: Could not get the electrode count!")
-
-def GetChannelNames(dataset):
-    if (dataset == "BNCI2014008"): #8 electrodes
-        return ["Fz", "Cz", "Pz", "Oz", "P3", "P4", "PO7","PO8"]
-    else:
-        print("Error: Could not get the channel names!")
-        
-def GetSubjectsCount(dataset):
-    if (dataset == "BNCI2014008"): #8 electrodes
-        return 8
-    else:
-        print("Error: Could not get subjects count!")
-
-#if "Cz" is provided then it will return an integer for this electrode
-def ElectrodeByName(dataset, electrode_name):   
-    i = 0
-    for e in GetChannelNames(dataset):
-        if e.lower() == electrode_name.lower():
-            return i
-        else:
-            i = i + 1
 
 #show average for a single channel and single subject
 def ShowPerSubjectDataset(dataset, channel, subject_show):
