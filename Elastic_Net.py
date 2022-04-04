909d4a4ccb888a5ed4623b30092a96e69c769593
Anton <toncho11@gmail.com>
2022-04-04 17:45:52 +0200

    Added elastic net.

---
+++ Elastic_Net.py
@@ -0,0 +1,13 @@
+
+
+# load and summarize the housing dataset
+from pandas import read_csv
+from matplotlib import pyplot
+# load dataset
+url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/housing.csv'
+dataframe = read_csv(url, header=None)
+# summarize shape
+print(dataframe.shape)
+# summarize first few lines
+print(dataframe.head())
+
