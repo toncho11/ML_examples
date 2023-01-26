e62bb8450aa878c64378435cee5f94bd67d7fa8f
toncho11 <toncho11@gmail.com>
2023-01-26 17:03:41 +0100

    Two classical GAN examples with comments and explanations.

--- GAN/GAN_Sin_Generator.py
+++ GAN/GAN_Sin_Generator.py
@@ -6,6 +6,25 @@ Source: https://realpython.com/generative-adversarial-networks/
     
 Generating sin samples
 
+D - descriminator
+G - generator
+
+The GAN training process consists of a two-player minimax game in which D is adapted to 
+minimize the discrimination error between real and generated samples, and G is adapted
+to maximize the probability of D making a mistake.
+
+Although the dataset containing the real data isn’t labeled, the training processes for D
+and G are performed in a supervised way. At each step in the training, D and G have their 
+parameters updated.
+
+To train D, at each iteration you label some real samples taken from the training data as
+1 and some generated samples provided by G as 0.
+
+For each batch of training data containing labeled real and generated samples, you update
+the parameters of D to minimize a loss function. After the parameters of D are updated, 
+you train G to produce better generated samples. The output of G is connected to D, whose 
+parameters are kept frozen 
+
 """
 
 import torch
@@ -22,25 +41,30 @@ device = torch.device("cpu")
 
 #1 Preparing the Training Data
 train_data_length = 1024
-train_data = torch.zeros((train_data_length, 2))
+train_data = torch.zeros((train_data_length, 2)) #a tensor with dimensions of 1024 rows and 2 columns
 train_data[:, 0] = 2 * math.pi * torch.rand(train_data_length)
 train_data[:, 1] = torch.sin(train_data[:, 0])
 train_labels = torch.zeros(train_data_length)
 train_set = [
     (train_data[i], train_labels[i]) for i in range(train_data_length)
-]
+] #the train set is a list with two tensors per item. First tensor contains x(random number) and sin(x). Second one is 0 and not used, required by PyTorch data loader.
 
 batch_size = 32
+#creating a pytorch data loader (and sampler)
 train_loader = torch.utils.data.DataLoader(
     train_set, batch_size=batch_size, shuffle=True
 )
 
+#plotting train data
+#plt.plot(train_data[:, 0], train_data[:, 1], ".")
+
 #Implementing the Discriminator
-class Discriminator(nn.Module):
+class Discriminator(nn.Module): #In PyTorch, the neural network models are represented by classes that inherit from nn.Module
     def __init__(self):
         super().__init__()
+        #The discriminator is a model with a two-dimensional input and a one-dimensional output.
         self.model = nn.Sequential(
-            nn.Linear(2, 256),
+            nn.Linear(2, 256), #The input is two-dimensional, and the first hidden layer is composed of 256 neurons with ReLU activation.
             nn.ReLU(),
             nn.Dropout(0.3),
             nn.Linear(256, 128),
@@ -49,11 +73,11 @@ class Discriminator(nn.Module):
             nn.Linear(128, 64),
             nn.ReLU(),
             nn.Dropout(0.3),
-            nn.Linear(64, 1),
-            nn.Sigmoid(),
+            nn.Linear(64, 1), #1 output
+            nn.Sigmoid(), #
         )
 
-    def forward(self, x):
+    def forward(self, x): #you use .forward() to describe how the output of the model is calculated.
         output = self.model(x)
         return output
 
@@ -65,11 +89,11 @@ class Generator(nn.Module):
     def __init__(self):
         super().__init__()
         self.model = nn.Sequential(
-            nn.Linear(2, 16),
+            nn.Linear(2, 16), #A model with a two-dimensional input, which will receive random points (z1, z2)
             nn.ReLU(),
             nn.Linear(16, 32),
             nn.ReLU(),
-            nn.Linear(32, 2),
+            nn.Linear(32, 2), #(in features, out features),a two-dimensional output that must provide (x1, xx2) points resembling those from the training data.
         )
 
     def forward(self, x):
@@ -81,20 +105,25 @@ generator = Generator()
 #Training the Models
 
 lr = 0.001
-num_epochs = 300
+num_epochs = 300 #how many repetitions of training using the whole training set will be performed
 loss_function = nn.BCELoss()
 
 optimizer_discriminator = torch.optim.Adam(discriminator.parameters(), lr=lr)
 optimizer_generator = torch.optim.Adam(generator.parameters(), lr=lr)
 
-#you need to implement a training loop in which training samples are fed to the models, and their weights are updated to minimize the loss function:
+#you need to implement a training loop in which training samples are fed to the models, and their weights are updated
+#to minimize the loss function:
 for epoch in range(num_epochs):
+    
     for n, (real_samples, _) in enumerate(train_loader):
+        
         # Data for training the discriminator
-        real_samples_labels = torch.ones((batch_size, 1))
+        real_samples_labels = torch.ones((batch_size, 1)) #real are labeled with 1
         latent_space_samples = torch.randn((batch_size, 2))
+        
         generated_samples = generator(latent_space_samples)
-        generated_samples_labels = torch.zeros((batch_size, 1))
+        generated_samples_labels = torch.zeros((batch_size, 1)) #generated are labeled with 0
+        
         all_samples = torch.cat((real_samples, generated_samples))
         all_samples_labels = torch.cat(
             (real_samples_labels, generated_samples_labels)
