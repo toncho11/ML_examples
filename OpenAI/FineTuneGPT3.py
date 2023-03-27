cd0a54cb59b6895d260eee5872c27362de043c36
toncho11 <toncho11@gmail.com>
2023-03-27 16:30:57 +0200

    Improved.

--- OpenAI/FineTuneGPT3.py
+++ OpenAI/FineTuneGPT3.py
@@ -8,6 +8,7 @@ This script shows how to fine-tune a GPT-3 model using Python with your
 own data for improved performance.
 The fine tuning is not done locally on your computer, but on the remote servers of OpenAI
 You will need an API access key.
+Extra code has been added that waits until the model is trained (or has failed)
 
 The data need to be a JSONL document with a new prompt and the ideal generated text:
 
@@ -19,6 +20,8 @@ https://platform.openai.com/docs/models/gpt-3
 Instruct models are optimized to follow single-turn instructions. Ada is the fastest model, 
 while Davinci is the most powerful.
 
+pip install openai
+
 """
 
 import json
@@ -29,7 +32,6 @@ import subprocess
 #api_key ="YOUR_OPENAI_API_KEY"
 keyFile = open('OpenAiApiKey.txt', 'r')
 api_key = keyFile.readline()
-
 openai.api_key = api_key
 
 # training_data = [{
@@ -40,12 +42,16 @@ openai.api_key = api_key
 #     "completion": " Visit you user settings in the left-hand side menu, then click 'upgrade account' button at the top.\n"
 # }]
 training_data = [{
-    "prompt": "What is your name ->",
-    "completion": " My name is Alfred.\n"
-},{
-    "prompt":"How old are you ->",
-    "completion": " I am 30 years old.\n"
-}]
+    "prompt": "What is your name? ->",
+    "completion": "My name is John.\n"
+   },{
+    "prompt":"How old are you? ->",
+    "completion": "I am thirty years old.\n"
+    }
+  ,{
+     "prompt":"How is your age? ->",
+     "completion": "My age is years old.\n"}
+  ]
 #Make sure to end each prompt with a suffix. According to the OpenAI API reference, you can use ->.
 #Make sure to end each completion with a suffix as well -  for example '\n'.
 
@@ -79,38 +85,55 @@ fine_tune_response = openai.FineTune.create(training_file=file_id)
 
 print("Started Training ...")
 id = fine_tune_response.id
+error = False
 while True:
     cmd = "openai -k " + api_key + " api fine_tunes.get -i " + id
     result = subprocess.run(cmd, stdout=subprocess.PIPE, shell=True)
     if "\"status\": \"succeeded\"" in str(result.stdout):
         print("Succeeded")
         break
-    if result.stderr !="" or "\"status\": \"failed\"" in str(result.stdout):
-        print("Error or failed job!")
+    if result.stderr !=None:
+        print("Error in cmd!")
+        error = True
+        break
+    if "\"status\": \"failed\"" in str(result.stdout):
+        print("Failed remote job!")
+        error = True
         break
     time.sleep(20)
     print("Still training ...")
 print("Done Training")
 
-retrieve_response = openai.FineTune.retrieve(id=fine_tune_response.id)
-
-#get the final trained model
-if fine_tune_response.fine_tuned_model != None:
-    fine_tuned_model = fine_tune_response.fine_tuned_model
+if error == False:
+    
+    retrieve_response = openai.FineTune.retrieve(id=fine_tune_response.id)
+    
+    #get the final trained model
+    if fine_tune_response.fine_tuned_model != None:
+        fine_tuned_model = fine_tune_response.fine_tuned_model
+    else:
+        fine_tuned_model = openai.FineTune.retrieve(id=fine_tune_response.id).fine_tuned_model
+    
+    if fine_tuned_model == None:
+        print("Model is empty!")
+    else:
+        #Testing
+        
+        #Next, use the completion with the fine-tuned model previously created:
+            
+        #Remember to end the prompt with the same suffix as we used in the training data; ->:
+        
+        prompts = ["What is your name? ->", "How old are you? ->"]
+        
+        for prompt in prompts:
+            answer = openai.Completion.create(
+              model=fine_tuned_model,
+              prompt=prompt,
+              max_tokens=100,
+              temperature=0
+            )
+            
+            print(answer['choices'][0]['text'])
+    
 else:
-    fine_tuned_model = openai.FineTune.retrieve(id=fine_tune_response.id).fine_tuned_model
-#Testing
-
-#Remember to end the prompt with the same suffix as we used in the training data; ->:
-
-new_prompt = "How old are you? ->"
-#Next, use the completion with the fine-tuned model previously created:
-
-answer = openai.Completion.create(
-  model=fine_tuned_model,
-  prompt=new_prompt,
-  max_tokens=100,
-  temperature=0
-)
-
-print(answer['choices'][0]['text'])
+    print("Error training model.")
