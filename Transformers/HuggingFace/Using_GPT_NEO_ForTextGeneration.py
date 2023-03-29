ef561a360dea38eaf2e5d33b8911531eaa63ab37
Anton <toncho11@gmail.com>
2023-03-29 17:35:20 +0200

    updated code that works correctly.

--- Transformers/HuggingFace/Using_GPT_NEO_ForTextGeneration.py
+++ Transformers/HuggingFace/Using_GPT_NEO_ForTextGeneration.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Mar 29 14:16:18 2023
+Created on Wed Mar 29 16:01:44 2023
+
+Source: https://huggingface.co/EleutherAI/gpt-neo-2.7B
 
 GPT-Neo 2.7B - a transformer model designed using EleutherAI's replication of the 
 GPT-3 architecture. The model is available on HuggingFace. Although it can be used
@@ -9,10 +11,28 @@ generating texts from a prompt.
 
 The task in this script is text generation.
 
+There is also a 1.3B and 6B versions.
+
 """
 
-from transformers import pipeline
+import torch
+from transformers import AutoTokenizer, GPTNeoForCausalLM
 
-generator = pipeline('text-generation', model='EleutherAI/gpt-neo-2.7B')
+tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neo-2.7B")
+model = GPTNeoForCausalLM.from_pretrained("EleutherAI/gpt-neo-2.7B")
 
-generator("EleutherAI has", do_sample=True, min_length=50)
+inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
+outputs = model(**inputs, labels=inputs["input_ids"])
+
+input_ids = inputs["input_ids"]
+
+gen_tokens = model.generate(
+    input_ids,
+    do_sample=True,
+    temperature=0.9,
+    max_length=100,
+)
+
+gen_text = tokenizer.batch_decode(gen_tokens)[0]
+
+print(gen_text)
