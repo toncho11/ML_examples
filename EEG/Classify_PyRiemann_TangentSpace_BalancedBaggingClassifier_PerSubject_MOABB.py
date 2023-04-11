7f7d8f91c854e33044b22595f4fb77493a0cbcf4
Anton <toncho11@gmail.com>
2023-04-11 09:42:47 +0200

    small update

--- EEG/Classify_PyRiemann_TangentSpace_BalancedBaggingClassifier_PerSubject_MOABB.py
+++ EEG/Classify_PyRiemann_TangentSpace_BalancedBaggingClassifier_PerSubject_MOABB.py
@@ -105,6 +105,7 @@ if __name__ == "__main__":
     #     LGBMClassifier()
     #  )
     
+    print("Processing ... ")
     #CrossSubjectEvaluation
     #WithinSessionEvaluation
     evaluation = CrossSubjectEvaluation(
