2501bd7448668563e99ee232b2eeead023ddcbb8
Anton <toncho11@gmail.com>
2023-04-11 21:58:15 +0200

    This version uses Accelerate and thus can be loaded on a regular laptop.

---
+++ Transformers/HuggingFace/ChatBots/ChatBotGPT_J_Human_Bot_With_Accelerate.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 11 21:42:20 2023
+
+@author: antona
+
+source: https://huggingface.co/docs/accelerate/usage_guides/big_modeling
+
+This version of the code uses Accelerate library and can be loaded in both CPU and GPU RAM.
+
+You clone the sharded version of this model with
+    
+git clone https://huggingface.co/sgugger/sharded-gpt-j-6B
+cd sharded-gpt-j-6B
+git-lfs install
+git lfs pull
+
+Then the model will be loaded from disk using the Accelerate library that
+can use both CPU RAM and GPU RAM in a process called offloading.
+
+"""
+
+from accelerate import init_empty_weights
+from transformers import AutoConfig, AutoModelForCausalLM
+
+checkpoint = "EleutherAI/gpt-j-6B"
+config = AutoConfig.from_pretrained(checkpoint)
+
+with init_empty_weights():
+    model = AutoModelForCausalLM.from_config(config)
+
+model.tie_weights()
+
+from accelerate import load_checkpoint_and_dispatch
+
+model = load_checkpoint_and_dispatch(
+    model, "sharded-gpt-j-6B", device_map="auto", no_split_module_classes=["GPTJBlock"],
+    offload_folder="offload"
+)
+
+from transformers import AutoTokenizer
+
+prompt = """This is a discussion between a [human] and a [robot]. 
+The [robot] is very nice and empathetic.
+
+[human]: Hello nice to meet you.
+[robot]: Nice to meet you too.
+###
+[human]: How is it going today?
+[robot]: Not so bad, thank you! How about you?
+###
+[human]: I am ok, but I am a bit sad...
+[robot]: Oh? Why that?
+###
+[human]: I broke up with my girlfriend...
+[robot]:"""
+
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+inputs = tokenizer(prompt, return_tensors="pt")
+inputs = inputs.to(0)
+output = model.generate(inputs["input_ids"])
+print(tokenizer.decode(output[0].tolist()))
