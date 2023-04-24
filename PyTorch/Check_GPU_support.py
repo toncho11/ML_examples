29edf652c1a847c47f3ce5eae4d274bdb2b016f2
Anton <toncho11@gmail.com>
2023-04-24 10:03:51 +0200

    improved script

--- PyTorch/Check_GPU_support.py
+++ PyTorch/Check_GPU_support.py
@@ -3,15 +3,16 @@
 Created on Thu Mar 30 15:08:59 2023
 
 @author: antona
+
+A script that tests if CUDA is usuable by PyTorch
 """
 
 import torch
 
-print(torch.cuda.is_available())
+print("Is CUDA available on PyTorch:", torch.cuda.is_available())
 
-print(torch.cuda.device_count())
+print("How many CUDA devices available: ",torch.cuda.device_count())
 
-print(torch.cuda.current_device())
+print("Current CUDA device ID (starts by 0):", torch.cuda.current_device())
 
-
-print(torch.cuda.get_device_name(0))
+print("CUDA device name:",torch.cuda.get_device_name(0))
