29d6bc6e3a9aaf0dd5ae583a4e11bd50a791cd98
Anton <toncho11@gmail.com>
2024-03-21 11:37:43 +0100

    Experimentation with Riemannian Minimum Distance to Means Field Classifier MDM-MF.

---
+++ EEG/MDM-MF/classifiy_P300_MDM_MF.py
@@ -0,0 +1,118 @@
+"""
+====================================================================
+Classification of P300 datasets from MOABB using MDM-MF
+====================================================================
+
+MDM-MF is the Riemammian Mimimum Distance to Means Field Classifier
+Paper: https://hal.science/hal-02315131
+
+"""
+# Author: Anton Andreev
+# Modified from plot_classify_EEG_tangentspace.py of pyRiemann
+# License: BSD (3-clause)
+
+from pyriemann.estimation import XdawnCovariances
+from sklearn.pipeline import make_pipeline
+from matplotlib import pyplot as plt
+import warnings
+import seaborn as sns
+from moabb import set_log_level
+from moabb.datasets import bi2013a
+from moabb.evaluations import WithinSessionEvaluation
+from moabb.paradigms import P300
+from pyriemann.classification import MDM, MeanField
+
+print(__doc__)
+
+##############################################################################
+# getting rid of the warnings about the future
+warnings.simplefilter(action="ignore", category=FutureWarning)
+warnings.simplefilter(action="ignore", category=RuntimeWarning)
+
+warnings.filterwarnings("ignore")
+
+set_log_level("info")
+
+##############################################################################
+# Create Pipelines
+# ----------------
+#
+# Pipelines must be a dict of sklearn pipeline transformer.
+
+##############################################################################
+# We have to do this because the classes are called 'Target' and 'NonTarget'
+# but the evaluation function uses a LabelEncoder, transforming them
+# to 0 and 1
+labels_dict = {"Target": 1, "NonTarget": 0}
+
+paradigm = P300(resample=128)
+
+datasets = [bi2013a()]  # MOABB provides several other P300 datasets
+
+# reduce the number of subjects, the Quantum pipeline takes a lot of time
+# if executed on the entire dataset
+n_subjects = 1
+for dataset in datasets:
+    dataset.subject_list = dataset.subject_list[0:n_subjects]
+
+overwrite = True  # set to True if we want to overwrite cached results
+
+pipelines = {}
+
+pipelines["XD+MDM_MF"] = make_pipeline(
+    # applies XDawn and calculates the covariance matrix, output it matrices
+    XdawnCovariances(
+        nfilter=3,
+        classes=[labels_dict["Target"]],
+        estimator="lwf",
+        xdawn_estimator="scm",
+    ),
+    MeanField(),
+)
+
+# this is a non quantum pipeline
+pipelines["XD+MDM"] = make_pipeline(
+    XdawnCovariances(
+        nfilter=3,
+        classes=[labels_dict["Target"]],
+        estimator="lwf",
+        xdawn_estimator="scm",
+    ),
+    MDM(),
+)
+
+print("Total pipelines to evaluate: ", len(pipelines))
+
+evaluation = WithinSessionEvaluation(
+    paradigm=paradigm, datasets=datasets, suffix="examples", overwrite=overwrite
+)
+
+results = evaluation.process(pipelines)
+
+print("Averaging the session performance:")
+print(results.groupby("pipeline").mean("score")[["score", "time"]])
+
+##############################################################################
+# Plot Results
+# ----------------
+#
+# Here we plot the results to compare the two pipelines
+
+fig, ax = plt.subplots(facecolor="white", figsize=[8, 4])
+
+sns.stripplot(
+    data=results,
+    y="score",
+    x="pipeline",
+    ax=ax,
+    jitter=True,
+    alpha=0.5,
+    zorder=1,
+    palette="Set1",
+)
+sns.pointplot(data=results, y="score", x="pipeline", ax=ax, palette="Set1")
+
+ax.set_ylabel("ROC AUC")
+ax.set_ylim(0.3, 1)
+
+plt.show()
