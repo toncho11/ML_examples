92a0768c2b498a4ef45729ef17b96df4df2a4549
Anton <toncho11@gmail.com>
2024-03-26 12:52:03 +0100

    Added a benchmark with all previous datasets.

--- EEG/MDM-MF/benchmark1.py
+++ EEG/MDM-MF/benchmark1.py
@@ -16,17 +16,37 @@ from sklearn.pipeline import make_pipeline
 from matplotlib import pyplot as plt
 import warnings
 import seaborn as sns
+import pandas as pd
 from moabb import set_log_level
-import moabb
+from moabb.datasets import (
+    bi2013a,
+    BNCI2014008,
+    BNCI2014009,
+    BNCI2015003,
+    EPFLP300,
+    Lee2019_ERP,
+    bi2014a,
+    bi2015a,
+    bi2015b,
+    EPFLP300
+)
 
-#12 MI datasets
-from moabb.datasets import BNCI2014_001, Zhou2016, BNCI2015_001, BNCI2014_002, BNCI2014_004, BNCI2015_004, AlexMI, Weibo2014, Cho2017, GrosseWentrup2009, PhysionetMI, Shin2017A
-    
-from moabb.evaluations import WithinSessionEvaluation
-from moabb.paradigms import LeftRightImagery, MotorImagery
-
+from moabb.datasets import (
+    BNCI2014_001, 
+    Zhou2016, 
+    BNCI2015_001, 
+    BNCI2014_002, 
+    BNCI2014_004, 
+    BNCI2015_004, 
+    AlexMI, 
+    Weibo2014, 
+    Cho2017, 
+    GrosseWentrup2009, 
+    PhysionetMI, 
+    Shin2017A
+)
 from moabb.evaluations import WithinSessionEvaluation, CrossSessionEvaluation, CrossSubjectEvaluation
-from moabb.paradigms import P300
+from moabb.paradigms import P300, MotorImagery
 from pyriemann.classification import MDM
 from enchanced_mdm_mf import MeanField
 from pyriemann.classification import MeanField as MeanField_orig
@@ -65,28 +85,38 @@ set_log_level("info")
 # to 0 and 1
 labels_dict = {"Target": 1, "NonTarget": 0}
 
-paradigm = MotorImagery(fmin=8,fmax=32)
-    
-#requires at least 3 subjects per dataset
-datasets = [Zhou2016(),
-            BNCI2014_001(),
-            #BNCI2015_001(), 
-            BNCI2014_002(), BNCI2014_004(), BNCI2015_004(), 
-            AlexMI(), Weibo2014(), Cho2017(), PhysionetMI(), 
-            Shin2017A(), GrosseWentrup2009()]
+paradigm_P300 = P300(resample=128,fmin=1, fmax=24)
+paradigm_MI   = MotorImagery(fmin=8,fmax=32)
 
-for d in datasets:
-    name = type(d).__name__
-    print(name)
-    if name not in [(lambda x: type(x).__name__)(x) for x in paradigm.datasets]:
-        print("Error: dataset not compatible with selected paradigm", name)
-        import sys
-        sys.exit(1)
+#name,    electrodes,   subjects
+#bi2013a	      16	24 (normal)                    
+#bi2014a    	  16	64 (usually low performance)
+#BNCI2014009	  16	10 (usually high performance)
+#BNCI2014008	   8	 8
+#BNCI2015003	   8	10
+#bi2015a          32    43
+#bi2015b          32    44
+   
+#datasets = [bi2013a(),BNCI2014008()] #bi2014a(),
+#datasets = [bi2013a(), BNCI2014008(), BNCI2014009(),BNCI2015003(), bi2014a()]
+#datasets = [bi2013a(), BNCI2014008(), BNCI2014009(),BNCI2015003(), bi2014a(), bi2015b()]
 
+#original 5 ds for P300
+datasets_P300 = [bi2013a(), BNCI2014008(), BNCI2014009(), BNCI2015003(), EPFLP300()]
+#original 12 ds for MI 
+#BNCI2015_001(), #not working
+datasets_MI = [ Zhou2016(),
+                BNCI2014_001(),          
+                BNCI2014_002(), BNCI2014_004(), BNCI2015_004(), 
+                AlexMI(), Weibo2014(), Cho2017(), PhysionetMI(), 
+                Shin2017A(), GrosseWentrup2009()
+                ]
 # reduce the number of subjects, the Quantum pipeline takes a lot of time
 # if executed on the entire dataset
-n_subjects = 3
-for dataset in datasets:
+n_subjects = 1
+for dataset in datasets_P300:
+    dataset.subject_list = dataset.subject_list[0:n_subjects]
+for dataset in datasets_MI:
     dataset.subject_list = dataset.subject_list[0:n_subjects]
 
 overwrite = True  # set to True if we want to overwrite cached results
@@ -94,8 +124,9 @@ overwrite = True  # set to True if we want to overwrite cached results
 pipelines = {}
 
 #power_means = [-1, -0.8, -0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1]
-power_means = [-1, -0.8, -0.6, -0.4, -0.2, -0.1, 0, 0.1, 0.2, 0.4, 0.6, 0.8, 1]
-#power_means = [-1, -0.75, -0.5, -0.25, -0.1, 0.1, 0.25, 0.5, 0.75, 1]
+#power_means = [-1, -0.8, -0.6, -0.4, -0.2, -0.1, 0, 0.1, 0.2, 0.4, 0.6, 0.8, 1]
+#original p
+power_means = [-1, -0.75, -0.5, -0.25, -0.1, -0.01, 0.01, 0.1, 0.25, 0.5, 0.75, 1]
 
 # pipelines["XD+MDM_MF_3PM"] = make_pipeline(
 #     # applies XDawn and calculates the covariance matrix, output it matrices
@@ -140,6 +171,22 @@ pipelines["MDM_MF_LDA"] = make_pipeline(
     LDA()
 )
 
+pipelines["MDM_MF_LR"] = make_pipeline(
+    # applies XDawn and calculates the covariance matrix, output it matrices
+    XdawnCovariances(
+        nfilter=3,
+        classes=[labels_dict["Target"]],
+        estimator="lwf",
+        xdawn_estimator="scm",
+    ),
+    #sum_means does not make a difference with 10 power means comapred to 3
+    MeanField(power_list=power_means,
+              method_label="sum_means", #not used if used as transformer
+              n_jobs=12,
+              ),
+    LogisticRegression(penalty="l1", solver="liblinear")
+)
+
 # pipelines["XD+MDM_MF_10PM_GPR"] = make_pipeline(
 #     # applies XDawn and calculates the covariance matrix, output it matrices
 #     XdawnCovariances(
@@ -156,6 +203,17 @@ pipelines["MDM_MF_LDA"] = make_pipeline(
 #     GaussianProcessRegressor(alpha = 0.1, kernel = RBF(length_scale_bounds = (0.1, 1.0)))
 # )
 
+# this is a non quantum pipeline
+pipelines["MDM"] = make_pipeline(
+    XdawnCovariances(
+        nfilter=3,
+        classes=[labels_dict["Target"]],
+        estimator="lwf",
+        xdawn_estimator="scm",
+    ),
+    MDM(),
+)
+
 pipelines["MDM_MF_SVM"] = make_pipeline(
     # applies XDawn and calculates the covariance matrix, output it matrices
     XdawnCovariances(
@@ -171,30 +229,27 @@ pipelines["MDM_MF_SVM"] = make_pipeline(
     svm.SVC(kernel="rbf")
 )
 
-# this is a non quantum pipeline
-pipelines["MDM"] = make_pipeline(
-    XdawnCovariances(
-        nfilter=3,
-        classes=[labels_dict["Target"]],
-        estimator="lwf",
-        xdawn_estimator="scm",
-    ),
-    MDM(),
-)
-
 print("Total pipelines to evaluate: ", len(pipelines))
 
-evaluation = WithinSessionEvaluation(
-    paradigm=paradigm, datasets=datasets, suffix="examples", overwrite=overwrite
+evaluation_P300 = WithinSessionEvaluation(
+    paradigm=paradigm_P300, datasets=datasets_P300, suffix="examples", overwrite=overwrite
+)
+evaluation_MI = WithinSessionEvaluation(
+    paradigm=paradigm_MI, datasets=datasets_MI, suffix="examples", overwrite=overwrite
 )
 
-results = evaluation.process(pipelines)
+results_P300 = evaluation_P300.process(pipelines)
+results_MI   = evaluation_MI.process(pipelines)
+
+results = pd.concat([results_P300, results_MI],ignore_index=True)
 
 print("Results:")
 print(results)
 
 print("Averaging the session performance:")
 print(results.groupby("pipeline").mean("score")[["score", "time"]])
+
+#save dataframe with results to disk for further analysis
 
 ##############################################################################
 # Plot Results
