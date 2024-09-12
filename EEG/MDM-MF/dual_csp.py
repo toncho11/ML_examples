259b2f025c1d892cee34f29394b489101ef2e5e2
Anton <toncho11@gmail.com>
2024-09-12 09:25:29 +0200

    update

---
+++ EEG/MDM-MF/dual_csp.py
@@ -0,0 +1,350 @@
+# -*- coding: utf-8 -*-
+"""
+
+====================================================================
+Classification of P300 and MI datasets from MOABB using MDM-MF
+====================================================================
+
+MDM-MF is the Riemammian Mimimum Distance to Means Field Classifier
+Paper: https://hal.science/hal-02315131
+
+Results:
+    
+@author: anton andreev
+"""
+
+from pyriemann.estimation import XdawnCovariances, ERPCovariances, Covariances
+from sklearn.pipeline import make_pipeline
+from enchanced_mdm_mf import MeanField as MeanFieldNew
+from pyriemann.classification import MeanField as MeanField_orig
+from moabb.evaluations import (
+    WithinSessionEvaluation,
+    CrossSessionEvaluation,
+    CrossSubjectEvaluation,
+)
+
+from pyriemann.classification import MDM
+import os
+from heavy_benchmark import benchmark_alpha, plot_stat
+from moabb import set_download_dir
+from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import MLPRegressor
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF
+from sklearn import svm
+from sklearn.discriminant_analysis import (
+    LinearDiscriminantAnalysis as LDA,
+    QuadraticDiscriminantAnalysis as QDA,
+)
+from sklearn.decomposition import PCA
+
+from sklearn.feature_selection import SelectFromModel
+from sklearn.svm import LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+from sklearn.preprocessing import PolynomialFeatures
+from enchanced_mdm_mf_tools import CustomCspTransformer, CustomCspTransformer2, CustomCspTransformer3, CustomCspTransformer4, CustomCspTransformer5,CustomCspTransformer6, CustomCspTransformer7
+from pyriemann.spatialfilters import CSP
+
+#start configuration
+hb_max_n_subjects = -1
+hb_n_jobs = -1
+hb_overwrite = False #if you change the MDM_MF algorithm you need to se to True
+mdm_mf_jobs = 1
+is_on_grid = False
+#end configuration
+
+if is_on_grid:
+    from mne import get_config, set_config
+    print("Changing MNE folder ...")
+    set_config('MNE_DATA', '/silenus/PROJECTS/pr-eeg-dl/antona/')
+    new_path = get_config("MNE_DATA")
+    print(f"The download directory is currently {new_path}")
+    print("Done changing MNE folder")
+    
+    pipeline_folder = "/home/antona/ML_examples/EEG/MDM-MF/pipelines/"
+else:
+    pipeline_folder = "C:\\Work\\PythonCode\\ML_examples\\EEG\\MDM-MF\\pipelines\\"
+
+pipelines = {}
+params_grid = None
+
+power_means = [-1, -0.75, -0.5, -0.25, -0.1, -0.01, 0.01, 0.1, 0.25, 0.5, 0.75, 1]
+
+power_means2 = [-1, -0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, -0.01, 0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+
+power_means3 = [-0.1, -0.01, 0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+
+power_means4 = [-1, -0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, -0.01, 0.01, 0.1]
+
+power_means5 = [-1, 0, 1]
+
+power_means6 = [-0.7, -0.01, 0.4, 0.8 ]
+
+power_means7 = [-1, 0.5, 0, 0.5, 1] #best so far
+
+power_means8 = [-1, 0.5, 0.001, 0.5, 1]
+
+power_means9 = [-1, -0.75, 0.5, 0.001, 0.5, 0.75, 1]
+
+power_means10 = [-1, -0.75, -0.5, -0.3, 0.001, 0.3, 0.5, 0.75, 1]
+
+#power_means11 = [-1, -0.75, -0.5, -0.25, -0.1, 0.001, 0.1, 0.25, 0.5, 0.75, 1]
+
+power_means11 = [-1, -0.75, -0.5, -0.25, -0.1, 0, 0.1, 0.25, 0.5, 0.75, 1]
+
+power_means12 = [-1, -0.75, -0.5, -0.25, -0.1, 0.001, 0.1, 0.25, 0.5, 0.75, 1]
+
+# power_means9 = [-1]
+
+# power_means10 = [1]
+
+# power_means11 = [0]
+
+#old best
+#CSP_10_A_E_A_PM12_LDA_CD_RO_2_5_D4_M50
+
+#-but has the fixed initalization (bug fix)
+#-but has riemmanian distance on p=0.001
+#-it has the outliers removal code updated to stop early
+#-the iterations are increased
+
+
+#new best
+pipelines["new_best_no_or"] = make_pipeline(
+    Covariances("oas"),
+    CustomCspTransformer(nfilter = 10),
+    MeanFieldNew(power_list=power_means11,
+              method_label="lda",
+              n_jobs=mdm_mf_jobs,
+              euclidean_mean         = False, #default = false
+              distance_strategy      = "default_metric",
+              remove_outliers        = False,
+              outliers_th            = 2.5,  #default = 2.5
+              outliers_depth         = 2,    #default = 4
+              max_outliers_remove_th = 50,   #default = 50
+              outliers_disable_mean  = False, #default = false
+              outliers_method        = "zscore",
+              zeta                   = 1e-07,
+              or_mean_init           = True,
+              ),   
+)
+
+pipelines["new_best_or"] = make_pipeline(
+    Covariances("oas"),
+    CustomCspTransformer(nfilter = 10),
+    MeanFieldNew(power_list=power_means11,
+              method_label="lda",
+              n_jobs=mdm_mf_jobs,
+              euclidean_mean         = False, #default = false
+              distance_strategy      = "default_metric",
+              remove_outliers        = True,
+              outliers_th            = 2.5,  #default = 2.5
+              outliers_depth         = 2,    #default = 4
+              max_outliers_remove_th = 50,   #default = 50
+              outliers_disable_mean  = False, #default = false
+              outliers_method        = "zscore",
+              zeta                   = 1e-07,
+              or_mean_init           = True,
+              ),   
+)
+
+# pipelines["CSP_dual"] = make_pipeline(
+#     Covariances("oas"),
+#     CSP(nfilter = 32, metric="euclid", log=False),
+#     CSP(metric = "ale", nfilter=10, log=False, maxiter = 2, n_iter_max = 2), # maxiter = 50, n_iter_max = 100)
+#     MeanFieldNew(power_list=power_means11,
+#               method_label="lda",
+#               n_jobs=mdm_mf_jobs,
+#               euclidean_mean         = False, #default = false
+#               distance_strategy      = "default_metric",
+#               remove_outliers        = False,
+#               outliers_th            = 2.5,  #default = 2.5
+#               outliers_depth         = 2,    #default = 4
+#               max_outliers_remove_th = 50,   #default = 50
+#               outliers_disable_mean  = False, #default = false
+#               outliers_method        = "zscore",
+#               zeta                   = 1e-07,
+#               or_mean_init           = True,
+#               ),   
+# )
+
+# Evaluation in %:
+#                    score      time
+# pipeline                          
+# CSP_dual        0.817947  0.323841
+# TSLR            0.823830  0.402167
+# new_best_no_or  0.817115  1.541570
+
+pipelines["CSP3_no_or"] = make_pipeline(
+    Covariances("oas"),
+    CustomCspTransformer3(),
+    MeanFieldNew(power_list=power_means11,
+              method_label="lda",
+              n_jobs=mdm_mf_jobs,
+              euclidean_mean         = False, #default = false
+              distance_strategy      = "default_metric",
+              remove_outliers        = False,
+              outliers_th            = 2.5,  #default = 2.5
+              outliers_depth         = 2,    #default = 4
+              max_outliers_remove_th = 50,   #default = 50
+              outliers_disable_mean  = False, #default = false
+              outliers_method        = "zscore",
+              zeta                   = 1e-07,
+              or_mean_init           = True,
+              ),   
+)
+
+pipelines["CSP3_or"] = make_pipeline(
+    Covariances("oas"),
+    CustomCspTransformer3(),
+    MeanFieldNew(power_list=power_means11,
+              method_label="lda",
+              n_jobs=mdm_mf_jobs,
+              euclidean_mean         = False, #default = false
+              distance_strategy      = "default_metric",
+              remove_outliers        = True,
+              outliers_th            = 2.5,  #default = 2.5
+              outliers_depth         = 2,    #default = 4
+              max_outliers_remove_th = 50,   #default = 50
+              outliers_disable_mean  = False, #default = false
+              outliers_method        = "zscore",
+              zeta                   = 1e-07,
+              or_mean_init           = True,
+              ),   
+)
+
+pipelines["CSP4_or"] = make_pipeline(
+    Covariances("oas"),
+    CustomCspTransformer4(),
+    MeanFieldNew(power_list=power_means11,
+              method_label="lda",
+              n_jobs=mdm_mf_jobs,
+              euclidean_mean         = False, #default = false
+              distance_strategy      = "default_metric",
+              remove_outliers        = True,
+              outliers_th            = 2.5,  #default = 2.5
+              outliers_depth         = 2,    #default = 4
+              max_outliers_remove_th = 50,   #default = 50
+              outliers_disable_mean  = False, #default = false
+              outliers_method        = "zscore",
+              zeta                   = 1e-07,
+              or_mean_init           = True,
+              ),   
+)
+
+pipelines["CSP5_or"] = make_pipeline(
+    Covariances("oas"),
+    CustomCspTransformer5(),
+    MeanFieldNew(power_list=power_means11,
+              method_label="lda",
+              n_jobs=mdm_mf_jobs,
+              euclidean_mean         = False, #default = false
+              distance_strategy      = "default_metric",
+              remove_outliers        = True,
+              outliers_th            = 2.5,  #default = 2.5
+              outliers_depth         = 2,    #default = 4
+              max_outliers_remove_th = 50,   #default = 50
+              outliers_disable_mean  = False, #default = false
+              outliers_method        = "zscore",
+              zeta                   = 1e-07,
+              or_mean_init           = True,
+              ),   
+)
+
+pipelines["CSP6_or"] = make_pipeline(
+    Covariances("oas"),
+    CustomCspTransformer6(),
+    MeanFieldNew(power_list=power_means11,
+              method_label="lda",
+              n_jobs=mdm_mf_jobs,
+              euclidean_mean         = False, #default = false
+              distance_strategy      = "default_metric",
+              remove_outliers        = True,
+              outliers_th            = 2.5,  #default = 2.5
+              outliers_depth         = 2,    #default = 4
+              max_outliers_remove_th = 50,   #default = 50
+              outliers_disable_mean  = False, #default = false
+              outliers_method        = "zscore",
+              zeta                   = 1e-07,
+              or_mean_init           = True,
+              ),   
+)
+
+pipelines["CSP7_or"] = make_pipeline(
+    Covariances("oas"),
+    CustomCspTransformer7(),
+    MeanFieldNew(power_list=power_means11,
+              method_label="lda",
+              n_jobs=mdm_mf_jobs,
+              euclidean_mean         = False, #default = false
+              distance_strategy      = "default_metric",
+              remove_outliers        = True,
+              outliers_th            = 2.5,  #default = 2.5
+              outliers_depth         = 2,    #default = 4
+              max_outliers_remove_th = 50,   #default = 50
+              outliers_disable_mean  = False, #default = false
+              outliers_method        = "zscore",
+              zeta                   = 1e-07,
+              or_mean_init           = True,
+              ),   
+)
+
+
+#can not use both
+AUG_Tang_SVM_standard       = False #Zhou2016 subject 4 can fail because of cov covariance estimator
+AUG_Tang_SVM_grid_search    = False #Zhou2016 subject 4 can fail because of cov covariance estimator
+TSLR = True
+
+from moabb.pipelines.utils import parse_pipelines_from_directory, generate_param_grid
+pipeline_configs = parse_pipelines_from_directory(pipeline_folder)
+
+if AUG_Tang_SVM_standard:
+    #no Grid search
+    for c in pipeline_configs:
+        if c["name"] == "AUG Tang SVM Grid":
+            pipelines["AD_TS_SVM_F"] = c["pipeline"]
+            params_grid = None
+            break
+      
+if AUG_Tang_SVM_grid_search:
+    for c in pipeline_configs:
+        if c["name"] == "AUG Tang SVM Grid":
+            pipelines["AD_TS_GS_SVM_F"] = c["pipeline"]
+            params_grid = generate_param_grid(pipeline_configs)
+            break
+
+if TSLR:
+    for c in pipeline_configs:
+        if c["name"] == "Tangent Space LR":
+            pipelines["TSLR"] = c["pipeline"]
+            break
+
+results = benchmark_alpha(pipelines, 
+                          params_grid = params_grid, 
+                          evaluation_type="withinsession",
+                          #evaluation_type="crosssubject", 
+                          max_n_subjects = hb_max_n_subjects, 
+                          n_jobs=hb_n_jobs, 
+                          overwrite = hb_overwrite,
+                          skip_P300 = True,
+                          skip_MI   = False,
+                          replace_x_dawn_cov_par_cov_for_MI=True
+                          )
+
+print("Results:")
+print(results)
+
+print("Averaging the session performance:")
+print(results.groupby("pipeline").mean("score")[["score", "time"]])
+
+# save results
+save_path = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), "results_dataframe_test.csv"
+)
+results.to_csv(save_path, index=True)
+
+print("Building statistic plots")
+plot_stat(results)
+
+#plot_stat(results, removeMI = True)
--- EEG/MDM-MF/enchanced_mdm_mf_tools.py
+++ EEG/MDM-MF/enchanced_mdm_mf_tools.py
@@ -20,6 +20,7 @@ from pyriemann.utils.utils import check_weights, check_function
 from pyriemann.utils.mean import mean_euclid, mean_riemann, mean_harmonic, _deprecate_covmats
 import warnings
 import scipy
+from sklearn import decomposition
 
 #requires a version of pyRiemann where CSP accepts maxiter and n_iter_max
 class CustomCspTransformer(BaseEstimator, TransformerMixin):
@@ -76,6 +77,14 @@ class CustomCspTransformer(BaseEstimator, TransformerMixin):
 
 class CustomCspTransformer2(BaseEstimator, TransformerMixin):
     
+    #for 128 electrodes ale is better than euclid but slower
+    # Averaging the session performance:
+    #                       score       time
+    # pipeline                              
+    # CSP_A_PM12_LDA_CD  0.853723  16.892366
+    # CSP_E_PM12_LDA_CD  0.823720   1.168123
+    # TSLR               0.875154   2.505664
+    # Difference is with 1 star confidence in favor of ALE
     def __init__(self, nfilter = 10): #maxiter_128 = 2, n_iter_max_128=2):
         #self.euclid = euclid
         self.nfilter = nfilter
@@ -84,20 +93,246 @@ class CustomCspTransformer2(BaseEstimator, TransformerMixin):
     
     def fit(self, X, y=None):
         #print("fit csp")
-        if X.shape[1] >= 60:
+        
+        self.n_electrodes = X.shape[1]
+        
+        if self.n_electrodes <= self.nfilter:
+            
+            self.csp = CSP(nfilter = self.nfilter,log=False)
+        
+        elif self.n_electrodes <= 64: #default < 60
+            
+            #n_iter_max=200 needs to be set in the code for "ale"
+            #good maxiter = 20, n_iter_max = 10
+            #5 5 ok
+            
+            #self.nfilter = 6
+            self.csp = CSP(metric = "ale", nfilter=self.nfilter, log=False, maxiter = 10, n_iter_max = 8) # maxiter = 50, n_iter_max = 100)
+        
+        else: #n_electrodes > 64
+            # if self.euclid:
+            #     self.csp = CSP(metric = "euclid", nfilter=self.nfilter, log=False) #pca par example
+            # else:
+            #self.nfilter = 10
             self.csp = CSP(metric = "ale", nfilter=self.nfilter, log=False, maxiter = 2 , n_iter_max = 2) #pca par example
-            self.csp.fit(X,y)
+            
+        self.csp.fit(X,y)
         
         return self
     
     def transform(self, X):
         
-        if X.shape[1] >= 60:
+        # if self.n_electrodes <= self.nfilter: #return unprocessed data
+        #     return X 
+        # else:
             X_transformed = self.csp.transform(X)
             return X_transformed
-        else:
-            return X
+
+class CustomCspTransformer3(BaseEstimator, TransformerMixin):
+    
+    def __init__(self):
         
+        self.nfilter = 10
+    
+    def fit(self, X, y=None):
+        
+        self.n_electrodes = X.shape[1]
+        
+        if self.n_electrodes <= self.nfilter:
+            self.csp = CSP(metric="euclid", log=False)
+            self.csp.fit(X,y)
+            return self
+        
+        elif self.n_electrodes > 32:
+            
+            self.csp1 = CSP(nfilter = 32, metric="euclid", log=False)
+            self.csp2 = CSP(metric = "ale", nfilter = self.nfilter, log=False, maxiter = 2, n_iter_max = 2) # maxiter = 50, n_iter_max = 100)
+            
+        else: #<=32
+            
+            self.csp1 = CSP(nfilter = self.nfilter, metric ="euclid", log=False)
+            self.csp2 = CSP(nfilter = self.nfilter, metric = "ale"  , log=False, maxiter = 2, n_iter_max = 2) # maxiter = 50, n_iter_max = 100)
+            
+        self.csp1.fit(X,y)
+        X1 = self.csp1.transform(X)
+        self.csp2.fit(X1,y)
+        
+        return self
+
+class CustomCspTransformer4(BaseEstimator, TransformerMixin):
+    
+    def __init__(self):
+        
+        self.nfilter = 10
+    
+    def fit(self, X, y=None):
+        
+        self.n_electrodes = X.shape[1]
+        
+        if self.n_electrodes <= self.nfilter:
+            self.csp = CSP(metric="euclid", log=False)
+            self.csp.fit(X,y)
+            return self
+        
+        elif self.n_electrodes > 32:
+            
+            self.csp1 = CSP(nfilter = 32, metric="euclid", log=False)
+            self.csp2 = CSP(metric = "ale", nfilter = self.nfilter, log=False, maxiter = 30, n_iter_max = 30) # maxiter = 50, n_iter_max = 100)
+            
+        else: #<=32
+            
+            self.csp1 = CSP(nfilter = self.nfilter, metric ="euclid", log=False)
+            self.csp2 = CSP(nfilter = self.nfilter, metric = "ale"  , log=False, maxiter = 30, n_iter_max = 30) # maxiter = 50, n_iter_max = 100)
+            
+        self.csp1.fit(X,y)
+        X1 = self.csp1.transform(X)
+        self.csp2.fit(X1,y)
+        
+        return self
+    
+    def transform(self, X):
+        
+        if self.n_electrodes <= self.nfilter:
+            
+            X_transformed = self.csp.transform(X)
+            return X_transformed
+        
+        else:
+            
+            X_transformed1 = self.csp1.transform(X)
+            X_transformed2 = self.csp2.transform(X_transformed1)
+            return X_transformed2
+
+class CustomCspTransformer5(BaseEstimator, TransformerMixin):
+    
+    def __init__(self):
+        
+        self.nfilter = 10
+    
+    def fit(self, X, y=None):
+        
+        self.n_electrodes = X.shape[1]
+        
+        if self.n_electrodes <= self.nfilter:
+            self.csp = CSP(metric="euclid", log=False)
+            self.csp.fit(X,y)
+            return self
+        
+        elif self.n_electrodes > 32:
+            
+            self.csp1 = CSP(nfilter = 32, metric="euclid", log=False)
+            self.csp2 = CSP(metric = "ale", nfilter = self.nfilter, log=False, maxiter = 50, n_iter_max = 100) # maxiter = 50, n_iter_max = 100)
+            
+        else: #<=32
+            
+            self.csp1 = CSP(nfilter = self.nfilter, metric ="euclid", log=False)
+            self.csp2 = CSP(nfilter = self.nfilter, metric = "ale"  , log=False, maxiter = 50, n_iter_max = 100) # maxiter = 50, n_iter_max = 100)
+            
+        self.csp1.fit(X,y)
+        X1 = self.csp1.transform(X)
+        self.csp2.fit(X1,y)
+        
+        return self
+    
+    def transform(self, X):
+        
+        if self.n_electrodes <= self.nfilter:
+            
+            X_transformed = self.csp.transform(X)
+            return X_transformed
+        
+        else:
+            
+            X_transformed1 = self.csp1.transform(X)
+            X_transformed2 = self.csp2.transform(X_transformed1)
+            return X_transformed2
+
+class CustomCspTransformer6(BaseEstimator, TransformerMixin):
+    
+    def __init__(self):
+        
+        self.nfilter = 10
+    
+    def fit(self, X, y=None):
+        
+        self.n_electrodes = X.shape[1]
+        
+        if self.n_electrodes <= self.nfilter:
+            self.csp = CSP(metric="euclid", log=False)
+            self.csp.fit(X,y)
+            return self
+        
+        else:
+            
+            self.csp1 = CSP(nfilter = self.nfilter, metric ="euclid", log=False)
+            #self.csp2 = CSP(nfilter = self.nfilter, metric = "ale"  , log=False, maxiter = 50, n_iter_max = 100) # maxiter = 50, n_iter_max = 100)
+            
+        self.csp1.fit(X,y)
+        #X1 = self.csp1.transform(X)
+        #self.csp2.fit(X1,y)
+        
+        return self
+    
+    def transform(self, X):
+        
+        if self.n_electrodes <= self.nfilter:
+            
+            X_transformed = self.csp.transform(X)
+            return X_transformed
+        
+        else:
+            
+            #X_transformed1 = self.csp1.transform(X)
+            X_transformed2 = self.csp1.transform(X)
+            return X_transformed2
+
+class CustomCspTransformer7(BaseEstimator, TransformerMixin):
+    
+    def __init__(self):
+        
+        self.nfilter = 10
+    
+    def fit(self, X, y=None):
+        
+        self.n_electrodes = X.shape[1]
+        
+        if self.n_electrodes <= self.nfilter:
+            self.csp = CSP(metric="euclid", log=False)
+            self.csp.fit(X,y)
+            return self
+        
+        elif self.n_electrodes > 32:
+            
+            self.csp1 = CSP(nfilter = 32, metric="euclid", log=False)
+            self.csp2 = CSP(metric = "ale", nfilter = self.nfilter, log=False, maxiter = 50, n_iter_max = 100) # maxiter = 50, n_iter_max = 100)
+            
+            self.csp1.fit(X,y)
+            X1 = self.csp1.transform(X)
+            self.csp2.fit(X1,y)
+            
+            return self
+        
+        else: #<=32
+            
+            #self.csp = CSP(nfilter = self.nfilter, metric ="euclid", log=False)
+            self.csp = CSP(nfilter = self.nfilter, metric = "ale"  , log=False, maxiter = 50, n_iter_max = 100) # maxiter = 50, n_iter_max = 100)
+            self.csp.fit(X,y)
+            return self
+        
+    
+    def transform(self, X):
+        
+        if self.n_electrodes <= self.nfilter or self.n_electrodes <=32:
+            
+            X_transformed = self.csp.transform(X)
+            return X_transformed
+        
+        else:
+            
+            X_transformed1 = self.csp1.transform(X)
+            X_transformed2 = self.csp2.transform(X_transformed1)
+            return X_transformed2
+
 class Diagonalizer(BaseEstimator, TransformerMixin):
     
     def __init__(self, diag_method="rjd", norm_method="L2", n_iter_max = 200):
